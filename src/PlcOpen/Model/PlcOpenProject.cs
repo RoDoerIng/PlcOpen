7433a795129cc8a87ed3ed6657432846351c08ea
RoDoerIng <ronnydoering.ing@gmail.com>
2017-08-09 10:01:35 +0200

    - added PlcOpenProject class

---
+++ src/PlcOpen/Model/PlcOpenProject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlcOpen.Model
+{
+    class PlcOpenProject
+    {
+        
+        private List<projectTypesDataType> dataTypes;
+
+        public List<projectTypesDataType> DataTypes
+        {
+            get { return dataTypes; }
+            set { dataTypes = value; }
+        }
+
+        private List<projectTypesPou> pous;
+
+        public List<projectTypesPou> Pous
+        {
+            get { return pous; }
+            set { pous = value; }
+        }
+
+
+    }
+}
